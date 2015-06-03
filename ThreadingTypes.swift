86836be763582823fd3ccdcc0f4b7c8cdce38015
Justin Spahr-Summers <justin@jspahrsummers.com>
2015-06-03 15:29:39 -0700

    Super dumb draft of a main-thread-only action

---
+++ ThreadingTypes.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+/// An arbitrary action that must be run on the main thread.
+struct UIAction<T> {
+	private let action: () -> T
+
+	init(_ action: () -> T) {
+		self.action = action
+	}
+
+	func enqueue() {
+		return enqueue { _ in () }
+	}
+
+	func enqueue(handler: T -> ()) {
+		dispatch_async(dispatch_get_main_queue()) {
+			let result = self.action()
+			handler(result)
+		}
+	}
+
+	func map<U>(transform: T -> U) -> UIAction<U> {
+		return UIAction<U> {
+			return transform(self.action())
+		}
+	}
+}
+
+let action = UIAction { println("foobar") }
+action.enqueue()
